d7af53aeeb00cc81504bba43dfe225ff33391185
Maha <maha@gmail.com>
2025-05-13 16:42:11 +0500

    cors added

--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
 import express from "express";
 import sequelize from "./db/db.connection.js";
 import CustomerRouter from "./router/router.js";
+import cors from "cors";
 
 const app = express();
 
 async function bootstrap() {
   try {
     await sequelize.authenticate();
-    sequelize.sync({ alter: true }).then(()=>{console.log("db connected");
-    }).catch((err)=>{console.log(`connection err ${err.message}`);
-    })
-    
-    app.use(express.json())
-    app.use(CustomerRouter)
+    sequelize
+      .sync({ alter: true })
+      .then(() => {
+        console.log("db connected");
+      })
+      .catch((err) => {
+        console.log(`connection err ${err.message}`);
+      });
 
-    
+    const corsOptions = {
+      origin: "http://localhost:5173",
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    };
+
+    app.use(cors(corsOptions));
+    app.use(express.json());
+
+    app.use(CustomerRouter);
+
     app.listen(8080, () => {
       console.log("http://localhost:8080");
     });
